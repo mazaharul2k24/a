18203194857a270fbc68d23df37f74938a747f07
mazaharul2k24 <159911942+mazaharul2k24@users.noreply.github.com>
2024-04-28 00:00:26 +0600

    Update IntroSec.jsx

--- app/components/IntroSec.jsx
+++ app/components/IntroSec.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { introSection } from '../lib/mazadev'
+import Link from 'next/link'
 
 export default function IntroSec() {
   return (
@@ -8,7 +9,7 @@ export default function IntroSec() {
         <div className="intiroWrapper py-20 px-5 block md:grid grid-cols-5 gap-5  ">
 
             <div className="intoleftContent md:col-span-2 ">
-            <Image alt='siteImage1' className='rounded-full mx-auto  shadow-md shadow-blue-800' src={introSection.intro_image} width={300} height={250}></Image>
+            <Image alt='intro image' className='rounded-full mx-auto  shadow-md shadow-blue-800' src={introSection.intro_image} width={300} height={250}></Image>
               
             </div>
 
@@ -16,8 +17,8 @@ export default function IntroSec() {
             <div className="introRightImg col-span-3 ">
               
                 <h2 className='text-2xl mt-5 font-semibold md:tex-4xl pb-3  my-2 pt-2  text-blue-500'>  {introSection.intro_title}</h2>
-                <h1 className='text-gray-400 leading-6'>{introSection.intro_desc}</h1>
-                <button className='bg-blue-700 text-white py-3 md:px-12 px-10  my-5 rounded-sm shadow-md  hover:bg-blue-800 hover:border-blue-800'>{introSection.intro_cv_dwn_btn}</button>
+                <h1 className='text-gray-400 leading-6'>{introSection.intro_desc}</h1><br /> <br />
+                <Link href={'/mazaharul_resume.pdf'} target='_blank'  className='bg-blue-700  text-white py-3 md:px-12 px-10  my-5 rounded-sm shadow-md  hover:bg-blue-800 hover:border-blue-800'>{introSection.intro_cv_dwn_btn}</Link>
 
             </div>
         </div>
